64bf61a97992d97702c5f440d318287b3cce1ef4
Daniel Le <dle@dacgroup.com>
2019-11-23 23:38:15 -0500

    Added logic that when given coordinates will give back the coordinates of the nearest parking spot

--- Navpark/main.py
+++ Navpark/main.py
@@ -1,8 +1,40 @@
 from flask import Flask, render_template, request
-
+import numpy as np
+import pandas as pd
 import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from sklearn.neighbors import KNeighborsClassifier
+import math
 
+def distance_calc(p1,p2):
+    return math.sqrt( ((p1[0]-p2[0])**2)+((p1[1]-p2[1])**2) )
 
+cred = credentials.Certificate('./london-data-47d52-9a3084ff0912.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+parkingDataCollname = "ParkingData"
+dataColl = db.collection(parkingDataCollname)
+latitudes = []
+longitudes = []
+groupIds = []
+
+#Set up the classifier object to assign new coordinates to a group
+groupCoords = db.collection(parkingDataCollname).limit(25).stream()
+for doc in groupCoords:
+    #TODO:Check for empty dictionaries
+    dictVal = doc.to_dict()
+    latitudes.append(dictVal['Latitude']) 
+    longitudes.append(dictVal['Longitude'])
+    groupIds.append(dictVal['GroupId'])
+coordArray = np.concatenate([np.array(latitudes).reshape(-1,1),np.array(longitudes).reshape(-1,1)],1)
+#Dataframe to store values
+df = pd.DataFrame(coordArray, columns = ['Latitude', 'Longitude'])
+df['GroupId'] = groupIds
+classifier = KNeighborsClassifier(n_neighbors=1)
+print(groupIds)
+classifier.fit(coordArray, groupIds)
 
 app = Flask(__name__)
 
@@ -11,7 +43,6 @@ app = Flask(__name__)
 @app.route('/')
 
 def home():
-
     return render_template('home.html')
 
 
@@ -23,6 +54,30 @@ def newStuff():
 	longitude = int(requestData["longitude"])
 	return {"newSum":longitude+latitude}
 
+@app.route('/get_parking',methods=['GET'])
+def get_parking():
+    #Get the request data: latitude and longitude
+    requestData = request.args
+    latitude = float(requestData["latitude"])
+    longitude = float(requestData["longitude"])
+    newCoords = np.array([latitude,longitude]).reshape(1,-1)
+    #Use the model to predict what group the coordinate belongs in
+    predicted_group = classifier.predict(newCoords)[0]
+    
+    #Get the coordinates within this group
+    coordsInGroup = df.loc[df['GroupId'] == predicted_group]
+    
+    #Find the coordinate in the group with the smallest distance
+    smallestDist = 10000
+    smallestDistCoord = (0,0)
+    for row in coordsInGroup.itertuples():
+        distance = distance_calc((42.986284,-81.240801),(row.Latitude,row.Longitude))
+        if distance < smallestDist:
+            smallestDist = distance
+            smallestDistCoord = (row.Latitude,row.Longitude)
+    return {"Parking_Coords":smallestDistCoord}
+    
+    
 if __name__ == "__main__":
 
     app.run(debug=True)
